02b6b7631f2203b1ca8f527f82d3ec8e8dfd24f9
Shabieh Hassan <shabieh50@gmail.com>
2023-02-19 14:30:49 -0600

    finished main page

--- app/src/main/java/com/example/beefit/MainActivity.java
+++ app/src/main/java/com/example/beefit/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //hides the ugly action bar that appears on top of the app.
+        getSupportActionBar().hide();
     }
 }
