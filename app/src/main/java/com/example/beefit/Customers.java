0e5dc473649e0534b3a4e88bf23732d2e6a13075
Shabieh Hassan <shabieh50@gmail.com>
2023-02-20 19:04:54 -0600

    initial commit

---
+++ app/src/main/java/com/example/beefit/Customers.java
@@ -0,0 +1,35 @@
+package com.example.beefit;
+
+public class Customers {
+
+private String Name;
+private String Email;
+private Integer Age;
+
+    public Customers() {
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public Integer getAge() {
+        return Age;
+    }
+
+    public void setAge(Integer age) {
+        Age = age;
+    }
+}
--- app/src/main/java/com/example/beefit/MainActivity.java
+++ app/src/main/java/com/example/beefit/MainActivity.java
@@ -3,15 +3,32 @@ package com.example.beefit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button SignUp, Login;
+    EditText Email, Pass;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         //hides the ugly action bar that appears on top of the app.
         getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SignUp = (Button) findViewById(R.id.signupbutton);
+        Login = (Button) findViewById(R.id.LoginButton);
+        Email = (EditText) findViewById(R.id.enterEmail);
+        Pass = (EditText) findViewById(R.id.getPassword);
+
+
     }
 }
