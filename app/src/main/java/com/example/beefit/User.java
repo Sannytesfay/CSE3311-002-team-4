13b846190615c5e67d8b9ca5d719756e63bcdafb
sannytesfay <tesfay.sunny@gmail.com>
2023-03-05 15:35:35 -0600

    sanny's commit v2

---
+++ app/src/main/java/com/example/beefit/User.java
@@ -0,0 +1,16 @@
+package com.example.beefit;
+
+public class User {
+
+    public String fullName, userName, age, email;
+
+    public User() {
+    }
+
+    public User(String fullName, String userName, String age, String email) {
+        this.fullName = fullName;
+        this.userName = userName;
+        this.age = age;
+        this.email = email;
+    }
+}
