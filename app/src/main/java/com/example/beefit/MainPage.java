8c431085fa6fb3762438fa63b7fe1e2ca02afa18
Shabieh Hassan <shabieh50@gmail.com>
2023-04-02 14:20:30 -0500

    Added stopwatch feature branch (not merged with main branch yet)

--- app/src/main/java/com/example/beefit/MainPage.java
+++ app/src/main/java/com/example/beefit/MainPage.java
@@ -2,13 +2,30 @@ package com.example.beefit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainPage extends AppCompatActivity {
+
+    ImageButton stopwatch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
+
+        stopwatch = (ImageButton) findViewById(R.id.Stopwatch_button);
+        stopwatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainPage.this, Stopwatch.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
